f9d4b4adf490a3ebd8791e81d5e4266a122e458e
Lautaro Rocha <lautarovrocha@gmail.com>
2023-02-19 20:20:48 -0300

    added reverse flex layout

---
+++ src/layouts/ReverseFlexLayout.jsx
@@ -0,0 +1,23 @@
+import Split from 'react-split'
+import { CssEditor, HtmlEditor, JsEditor, Result} from "../components"
+
+const ReverseFlexLayout = () => {
+    return (
+        <Split
+            className="split content"
+            gutterAlign="end"
+        >
+            <Result />
+            <Split
+                className="split-editors"
+                direction="vertical"
+                >
+                    <HtmlEditor/>            
+                    <CssEditor/>
+                    <JsEditor/>
+            </Split>            
+        </Split>
+    );
+}
+
+export default ReverseFlexLayout;
