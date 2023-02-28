00293562c8e68dcf0d87d4b30ff01570f60b452e
Lautaro Rocha <lautarovrocha@gmail.com>
2023-02-28 17:38:47 -0300

    getting saved pens and setting state

--- src/components/PensGallery/PensGallery.jsx
+++ src/components/PensGallery/PensGallery.jsx
@@ -1,25 +1,32 @@
 import { readFromDatabase } from "../../services/readFromDatabase"
 import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { setPens } from "../../redux/pensSlice";
 
 function PensGallery() {
+
+    const dispatch = useDispatch()
+    const pens = useSelector((state) => state.pens.savedPens)
 
     const getPens = async()=> {
         try{
             const snapshotValue = await readFromDatabase();
-            console.log("Snapshot value:", snapshotValue);
+            console.log(Object.values(snapshotValue))
+            dispatch(setPens(Object.values(snapshotValue)));
         } catch (error) {
             console.error("Error fetching data:", error);
-            // Handle the error
         }
     } 
 
     useEffect(()=>{
         getPens()
     }, [])
-
-
+    
+    
   return (
-    <div>PensGallery</div>
+    <div>
+        {pens && pens.map( (pen, idx) => {return(<h2 key={idx}>{pen.title}</h2>)})}
+    </div>
   )
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import codeReducer from './codeSlice'
 import layoutReducer from './layoutSlice'
 import navigationSlice from './navigationSlice'
+import pensSlice from './pensSlice'
 
 export const store = configureStore({
   reducer: {
     code : codeReducer,
     layout : layoutReducer,
-    navigation : navigationSlice
+    navigation : navigationSlice,
+    pens: pensSlice
   },
 })
